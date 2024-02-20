b3192a7c254d9f2ded8223915ab858660da3ba8c
Yalle1 <ensaryalap1@outlook.com>
2024-02-20 06:23:04 +0100

    Created Player and its Sub-Classes

--- m1-generics/src/main/java/com/cydeo/Main.java
+++ m1-generics/src/main/java/com/cydeo/Main.java
@@ -18,6 +18,21 @@ public class Main {
 
         printDoubled(items);
 
+        System.out.println("----------------------------------");
+
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer beckham = new SoccerPlayer("Beckham");
+
+        Team<SoccerPlayer> liverpool = new Team("Liverpool");
+
+        liverpool.addPlayer(beckham);
+
+
+
+
+
+
     }
 
     private static void printDoubled(ArrayList<Integer> items) {
